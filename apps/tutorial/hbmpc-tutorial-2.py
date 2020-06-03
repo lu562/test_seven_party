88b2804449418c7bf798e871472cd7a58d04c372
Donghang Lu <lu562@purdue.edu>
2020-06-02 23:40:00 -0400

    Update hbmpc-tutorial-2.py

--- apps/tutorial/hbmpc-tutorial-2.py
+++ apps/tutorial/hbmpc-tutorial-2.py
@@ -761,7 +761,7 @@ def triple_generation_for_multi_matrix(ctx, k, n):
 
 async def simple_matrix(ctx, **kwargs):
     k = kwargs["k"]
-    n = 8
+    n = 128
     matrix_a = [[ctx.Share(3) for _ in range(k)] for _ in range(k)]
     matrix_b = [[ctx.Share(5) for _ in range(k)] for _ in range(k)]
     R, R_inverse = offline_multi_matrix_multiply(ctx, k, n)
