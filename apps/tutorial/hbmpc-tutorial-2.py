140b6580c88c8150d23d2b8daab161f4a52b0e63
Donghang Lu <lu562@purdue.edu>
2020-07-02 17:44:26 -0400

    Update hbmpc-tutorial-2.py

--- apps/tutorial/hbmpc-tutorial-2.py
+++ apps/tutorial/hbmpc-tutorial-2.py
@@ -789,7 +789,7 @@ async def simple_matrix(ctx, **kwargs):
     global total_communicate_time
     k = kwargs["k"]
     n = 1
-    p = 16
+    p = 1024
     matrix_a = [[ctx.Share(3) for _ in range(k)] for _ in range(k)]
     matrix_b = [[ctx.Share(5) for _ in range(k)] for _ in range(k)]
     await run_command_sync("chmod 777 ./apps/tutorial/cpp/multi_matrix_add")
@@ -838,7 +838,7 @@ if __name__ == "__main__":
     asyncio.set_event_loop(asyncio.new_event_loop())
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
-    k = 40
+    k = 20
     try:
         # pp_elements = FakePreProcessedElements()
         # # k = 3 # How many of each kind of preproc
